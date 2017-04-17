481e94ba4885b960f39dc7455b89a0afdca0b97a
Matej Mazur <matej.mazur@mail.com>
2017-04-18 01:02:37 +0200

    Minor cosmetic changes in events

--- events.js
+++ events.js
@@ -1,14 +1,17 @@
 events.publish('init')
+
 events.on('init',                   types.logic.giveTypes)
 events.on('typesGived',             types.dom.loadType)
 events.on('typeLoaded',             types.dom.listenType)
 events.on('typeClicked',            types.logic.setType)
+
 events.on('typeDone',               marks.dom.loadMarks)
 events.on('marksLoaded',            marks.dom.listenMarks)
 events.on('marksLoaded',            marks.dom.listenSwitcher)
 events.on('marksSwitched',          marks.dom.switchMarks)
 events.on('marksClicked',           marks.logic.setMarks)
-events.on('marksDone',              game.logic.setGame) // + 25, 31
+
+events.on('marksDone',              game.logic.setGame) // + 28, 34
 events.on('gameCreated',            game.dom.loadGame)
 events.on('gameLoaded',             game.dom.listenGame)
 events.on('gameClicked',            game.logic.handleClick)
