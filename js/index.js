2ec4bd3dcb8663ad97464430436b1c3f3c95f033
Matej Mazur <matej.mazur@mail.com>
2017-04-21 07:32:20 +0200

    Change idea

--- js/index.js
+++ js/index.js
@@ -1,108 +1,68 @@
-const init = (() => {
-
-  // Bind events *******************************
-  events.on('init', loadType)
-  events.on('typeDone', loadMarks)
-  events.on('marksDone', loadGame)
-
-
-  // Loaders ***********************************
-
-  // Type of game
-  function loadType() {
-    const templateType = document.getElementById('template-types-of-game').innerHTML
-    const data = {
-      types: [
-        ['Human', 'Human'],
-        ['Human', 'PC']
-      ]
-    }
-    loadTemplate(templateType, data)
-
-    const loadedType = document.getElementById('js-type')
-    loadedType.addEventListener('click', ({ target }) => {
-      const parent = target.parentNode
-      if (parent.className.indexOf('js-option') === -1) return
-      setType(data, parent)
-    })
-
-    function setType(data, parent) {
-      const index = parent.dataset.index
-      return events.publish('typeDone', data.types[index])
-    }
+const game = (() => {
+  const state = {
+    type: undefined,
+    marks: undefined,
+    board: new Array(9).fill(null),
+    currentPlayer: Math.round(Math.random())
   }
+  const winLines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ]
 
-  // Marks of players
-  function loadMarks(type) {
-    const templateMarks = document.getElementById('template-marks').innerHTML
-    const data = {
-      type,
-      marks: ['X', 'O'],
-      score: [0, 0]
-    }
-    loadTemplate(templateMarks, data)
-
-    const loadedMarks = document.getElementsByClassName('js-mark')
-    const loadedSwitcher = document.getElementById('js-switch-marks')
-    const loadedStarter = document.getElementById('js-load-game')
-    loadedSwitcher.addEventListener('click', switchMarks(loadedMarks, data.marks))
-    loadedStarter.addEventListener('click', () => 
-    events.publish('marksDone', data))
+  function setType(index) {
+    const types = [
+      ['Human', 'Human'],
+      ['Human', 'PC']
+    ]
     
-    function switchMarks(loadedMarks, data) {
-      return () => {
-        const [a, b] = [...loadedMarks].map(({ innerHTML }) => innerHTML)
-        const switchedMarks = switcher(a, b)
-        switchedMarks.map((mark, index) => loadedMarks[index].innerHTML = mark)
-        data = switchedMarks
-      }
-    }
+    return state.type = types[index]
   }
 
-  // Game
-  function loadGame(players) {
-    const templateGame = document.getElementById('template-game').innerHTML
-    const state = {
-      players,
-      board: new Array(9).fill(null),
-      currentPlayer: players.marks[Math.round(Math.random())]
-    }
-    loadTemplate(templateGame, state)
+  function setMarks(index) {
+    const marks = [
+      ['X', 'O'],
+      ['O', 'X']
+    ]
     
-    const loadedBoard = document.getElementById('js-board')
-    const loadedStatus = document.getElementById('js-status')
-    events.on('stateChanged', load(loadedStatus))
-    loadedBoard.addEventListener('click', ({ target }) => {
+    return state.marks = marks[index]
+  }
 
-      if (target.className.indexOf('js-field') === -1) return
-      const domField = target
-      const index = field.dataset.index
-      const stateField = state.board[index]
+  function play(index) {
+    let player = state.currentPlayer
+    const mark = state.marks[player]
+    const board = state.board
+    board[index] = mark
 
-      if (!isEmpty(stateField)) return
-      setState()
-
-    })
-
-    function setState(newValues) {
-      Object.assign(state, newValues)
-      events.publish('stateChanged', state)
+    if (control({ board, mark, index }) === 3) {
+      return console.log(`${state.marks[player]} wins!`)
+    } else {
+      return state.currentPlayer = player === 1 ? 0 : 1
     }
   }
 
-
-  // Helpers ***********************************
-
-  function loadTemplate(template, data) {
-    const target = document.getElementById('js-main')
-    target.innerHTML = Handlebars.compile(template)(data)
+  function control({ board, mark, index }) {
+    const lines = winLines.filter(line => line.indexOf(index) !== -1)
+    const output = lines.filter(line => {
+      return line.map(index => board[index] === mark)
+    })
+    return output
   }
-
-
-  // Public interface **************************
 
   return {
-
+    setType,
+    setMarks,
+    play,
+    state
   }
 })()
 
+// game.setType(1)
+// game.setMarks(0)
+// game.play(0 - 8)
